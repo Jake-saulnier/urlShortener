5d53e1fdcb86c6efba4b31c3b0b437735d4a7991
Jake Saulnier <jake.saulnier@icloud.com>
2026-08-08 20:51:24 +0100

    added functionality for given urls to be converted to base 36 code

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const app = express();
+app.use(express.json());
 const PORT = 3000;
+const urls = new Map();
 
 app.get("/api/health", (req, res) => {
-    res.json({ status: "ok" });
+    res.json({ 
+        status: "ok" 
+    });
 });
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.post("api/urls", (req, res) => {
+    const originalUrl = req.body.url;
+
+    const shortCode = Math.random().toString(36).substring(2, 8);
+    urls.set(shortCode, originalUrl);
+
+    res.json({
+        shortCode: shortCode,
+    });
 });
